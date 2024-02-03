bcf0d64bd5b243b33d63934e1171a7f3082fd0d3
CHAOS 131 <131.chaos@gmail.com>
2024-02-03 16:36:44 -0500

    Quick test for intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,11 +14,13 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.commands.DriveToLocation;
 import frc.robot.commands.DriverRelativeDrive;
 import frc.robot.commands.ResetPosition;
 import frc.robot.commands.RobotRelativeDrive;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.swerve.SwerveDrive2022;
 import frc.robot.subsystems.swerve.SwerveDrive2024;
 
@@ -32,6 +34,8 @@ public class RobotContainer {
     ? SwerveDrive2022.createSwerveDrive() 
     : SwerveDrive2024.createSwerveDrive();
 
+  private Intake m_Intake = new Intake();
+
   public RobotContainer() {
     configureBindings();
 
@@ -42,6 +46,7 @@ public class RobotContainer {
 
   private void configureBindings() {
     m_swerveDrive.setDefaultCommand(new DriverRelativeDrive(m_driver, m_swerveDrive));
+    m_Intake.setDefaultCommand(new RunCommand(()-> m_Intake.runSpeed(0.0), m_Intake));
     m_driver.a().onTrue(new InstantCommand(() -> m_swerveDrive.recalibrateModules()));
     m_driver.povUp().onTrue(new InstantCommand(() -> m_swerveDrive.resetHeading(Rotation2d.fromDegrees(0))));
     m_driver.b().whileTrue(new InstantCommand(() -> m_swerveDrive.resetPose(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))).andThen( new DriveToLocation(new Pose2d(1, 0, Rotation2d.fromDegrees(0)), m_swerveDrive)));
@@ -55,6 +60,7 @@ public class RobotContainer {
          BaseSwerveDrive.RotationSpeedModifier = 1.0;
       } 
     ));
+    m_driver.rightBumper().whileTrue(new RunCommand(()-> m_Intake.runSpeed(0.3), m_Intake));
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,17 +2,24 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-	protected TalonFX m_motor;
-	protected TalonFXConfiguration m_config;
 	
+	CANSparkMax m_intakeUpper = new CANSparkMax(42,MotorType.kBrushless);
+	CANSparkMax m_intakeLower = new CANSparkMax(43,MotorType.kBrushless);
 	public Intake() {
-		m_motor = new TalonFX(0);
-		m_config = new TalonFXConfiguration();
+		//m_motor = new TalonFX(0);
+		//m_config = new TalonFXConfiguration();
 
-		m_motor.getConfigurator().apply(m_config);
+		//m_motor.getConfigurator().apply(m_config);
+	}
+	public void runSpeed(double speed){
+		m_intakeUpper.set(speed);
+		m_intakeLower.set(speed);
 	}
 }
