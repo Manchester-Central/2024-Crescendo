c68231b9c090dcdc9b92fc7f9300889f7b6ad147
CHAOS 131 <131.chaos@gmail.com>
2024-02-05 20:38:25 -0500

    Added state machine to vision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
 public class Constants {
-	public static final boolean Use2022Robot = false;
+	public static final boolean Use2022Robot = true;
+
+	public static class VisionConstants {
+		public static final int[] AprilTagPipelines = {0, 1};
+	}
 }
