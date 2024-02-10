b8438860fb36ec638a603c6246f44941630c34be
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2024-02-10 09:27:47 -0500

    Changed Robot reletive drive back to full contorls

--- src/main/java/frc/robot/commands/RobotRelativeDrive.java
+++ src/main/java/frc/robot/commands/RobotRelativeDrive.java
@@ -29,8 +29,8 @@ public class RobotRelativeDrive extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // m_swerveDrive.moveRobotRelative(m_driver.getLeftY(), -m_driver.getLeftX(), -m_driver.getRightX());
-    m_swerveDrive.moveRobotRelative(m_driver.getLeftY(), 0, 0);
+    m_swerveDrive.moveRobotRelative(m_driver.getLeftY(), -m_driver.getLeftX(), -m_driver.getRightX());
+    //m_swerveDrive.moveRobotRelative(m_driver.getLeftY(), 0, 0);
 
   }
 
