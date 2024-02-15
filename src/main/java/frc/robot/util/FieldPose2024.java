7723ffed0ed2a7c735fe39ffcdbe456f1fe56347
Colin Carlson <nhlaxb@gmail.com>
2024-02-14 19:15:04 -0500

    Values need to be fixed

--- src/main/java/frc/robot/util/FieldPose2024.java
+++ src/main/java/frc/robot/util/FieldPose2024.java
@@ -15,7 +15,16 @@ public class FieldPose2024 extends MirroredDrivePose {
     public static final double FieldWidthMeters = 16.512;
     public static final Alliance DefaultAlliance = Alliance.Blue;
 
-    public static final FieldPose2024 Speaker = new FieldPose2024("Speaker", new Pose2d(-0.0381, 5.547868, Rotation2d.fromDegrees(0)));
+    // The April Tag point at the Amp, accounting for robot orientation but not robot size
+    public static final FieldPose2024 Amp           = new FieldPose2024("Amp", new Pose2d(1.842, 8.204, Rotation2d.fromDegrees(270)));
+    // TODO
+    public static final FieldPose2024 Source        = new FieldPose2024("Source", new Pose2d(0,0, Rotation2d.fromDegrees(0)));
+    // The rotation is the general angle the robot should be facing to see the speaker
+    public static final FieldPose2024 Speaker       = new FieldPose2024("Speaker", new Pose2d(-0.0381, 5.547868, Rotation2d.fromDegrees(180)));
+    // 
+    public static final FieldPose2024 StageLeft     = new FieldPose2024("StageLeft", new Pose2d(0,0, Rotation2d.fromDegrees(0)));
+    public static final FieldPose2024 StageCenter   = new FieldPose2024("StageCenter", new Pose2d(0,0, Rotation2d.fromDegrees(0)));
+    public static final FieldPose2024 StageRight    = new FieldPose2024("StageRight", new Pose2d(0,0, Rotation2d.fromDegrees(0)));
 
     public FieldPose2024(String name, Pose2d bluePose) {
         super(FieldWidthMeters, DefaultAlliance, name, bluePose);
