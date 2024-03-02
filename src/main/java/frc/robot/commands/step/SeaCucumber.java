be447b4e7239e3c3951e432a62df180667ed21f6
CHAOS 131 <131.chaos@gmail.com>
2024-03-02 09:54:33 -0500

    Added the "Sea Cucumber" with auto

---
+++ src/main/java/frc/robot/commands/step/SeaCucumber.java
@@ -0,0 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.step;
+
+import com.chaos131.auto.ParsedCommand;
+import com.chaos131.swerve.BaseSwerveDrive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.LauncherConstants;
+import frc.robot.Constants.LiftConstants;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Launcher;
+import frc.robot.subsystems.Lift;
+
+// TODO: Implement actual control logic
+public class SeaCucumber extends Command {
+  private Intake m_intake;
+  private Lift m_lift;
+  private Launcher m_launcher;
+  private Feeder m_feeder;
+
+  /** Creates a new SeaCucumber. */
+  public SeaCucumber(Intake intake, Lift lift, Feeder feeder, Launcher launcher) {
+    m_intake = intake;
+    m_lift = lift;
+    m_launcher = launcher;
+    m_feeder = feeder;
+    addRequirements(intake, lift, feeder, launcher);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_lift.moveToHeight(LiftConstants.SeaCucumberHeightMeters);
+
+    m_launcher.setTiltAngle(LauncherConstants.SeaCucumberAngle);
+
+    m_feeder.setFeederPower(1.0);
+
+    m_launcher.setLauncherRPM(650, 650);
+
+    //if (m_lift.atTargetHeight(LiftConstants.IntakeHeightMeters) && m_launcher.atTargetAngle(LauncherConstants.IntakeAngle)) {
+    var atHeight = m_lift.atTargetHeight(LiftConstants.SeaCucumberHeightMeters);
+    var hasPiece = m_feeder.hasNote();
+    if (atHeight && !hasPiece && m_launcher.atTargetAngle(LauncherConstants.SeaCucumberAngle)) {
+      m_intake.setIntakePower(1);
+    }else{
+      m_intake.setIntakePower(0);
+    }
+
+  }
+
+  public static Command createAutoCommand(ParsedCommand parsedCommand, Intake intake, Lift lift, Feeder feeder, Launcher launcher){
+    return new SeaCucumber(intake, lift, feeder, launcher);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_intake.setIntakePower(0);
+    m_launcher.setLauncherPower(0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
