0f1d8ef9018a8932395985762dd78cf7e07745d9
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2024-03-28 19:50:46 -0400

    Green Beans & Cassarole

---
+++ src/main/java/frc/robot/subsystems/LightStrip.java
@@ -0,0 +1,56 @@
+package frc.robot.subsystems;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.DriveDirection;
+
+public class LightStrip extends SubsystemBase{
+    private AddressableLED m_leds;  
+    private AddressableLEDBuffer m_buffer;
+    private final int NumLeds = 0;
+    private Object m_mutex = new Object();
+    private Supplier<Boolean> m_intakeSupplier, m_feederSupplier;
+
+    public LightStrip(Supplier<Boolean> intakeHasNote, Supplier<Boolean> feederHasNote) {
+        m_intakeSupplier = intakeHasNote;
+        m_feederSupplier = feederHasNote;
+        m_leds.setLength(NumLeds);
+        m_buffer = new AddressableLEDBuffer(NumLeds);
+    }
+
+    public void setSingleColor(int red, int green, int blue) {
+        for(int i = 0; i < m_buffer.getLength(); i++) {
+            m_buffer.setRGB(i, red, green, blue);
+        }
+
+        m_leds.setData(m_buffer);
+    }
+
+    @Override
+    public void periodic() {
+        // If the robot is disabled, display the current alliance if there is one
+        if(DriverStation.isDisabled()) {
+            if(DriverStation.getAlliance().isPresent()) {
+                if(DriverStation.getAlliance().get() == Alliance.Red) {
+                    setSingleColor(255, 0, 0); // Set all LEDs to red if we are disabled on red
+                } else {
+                    setSingleColor(0, 0, 255);
+                }
+            }
+        } else {
+            // Logic for an enabled robot
+            if(m_intakeSupplier.get() == true) {
+                setSingleColor(255, 255, 0); // Yellow
+            } else if(m_feederSupplier.get() == true && m_intakeSupplier.get() == false) {
+                setSingleColor(0, 255, 0); // Green
+            } else {
+                setSingleColor(255, 255, 255); // White
+            }
+        }
+    }
+}
