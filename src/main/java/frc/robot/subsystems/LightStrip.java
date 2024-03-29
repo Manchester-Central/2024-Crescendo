00caa07a89cdc1e190135c3ed9dbf5a796f7ae10
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2024-03-28 20:42:15 -0400

    Added color enum and fixed some formatting

--- src/main/java/frc/robot/subsystems/LightStrip.java
+++ src/main/java/frc/robot/subsystems/LightStrip.java
@@ -16,12 +16,62 @@ public class LightStrip extends SubsystemBase{
     private Supplier<Boolean> m_intakeSupplier;
     private Supplier<Boolean> m_feederSupplier;
 
+    public enum Color {
+        RED (255, 0, 0),
+        GREEN(0, 255, 0),
+        BLUE (0, 0, 255),
+        CHAOS_ORANGE (255, 30, 0),
+        OFF (0, 0, 0),
+        YELLOW (255, 255, 0),
+        WHITE(255, 255, 255);
+        
+        int red;
+        int green;
+        int blue;
+
+        Color(int r, int g, int b) {
+            red = r;
+            green = g;
+            blue = b;
+        }
+    }
+
     public LightStrip(Supplier<Boolean> intakeHasNote, Supplier<Boolean> feederHasNote) {
         m_intakeSupplier = intakeHasNote;
         m_feederSupplier = feederHasNote;
+        m_leds = new AddressableLED(NumLeds);
         m_leds.setLength(NumLeds);
         m_buffer = new AddressableLEDBuffer(NumLeds);
         m_leds.start();
+    }
+
+    @Override
+    public void periodic() {
+        // Set color to orange if the robot is not connected to the driver station
+        if(DriverStation.isDSAttached() == false) {
+            setSingleColor(Color.CHAOS_ORANGE);
+            return;
+        }
+
+        // If the robot is disabled, display the current alliance if there is one
+        if(DriverStation.isDisabled()) {
+            if(DriverStation.getAlliance().isPresent()) {
+                if(DriverStation.getAlliance().get() == Alliance.Red) {
+                    setSingleColor(Color.RED);
+                } else {
+                    setSingleColor(Color.BLUE);
+                }
+            }
+        } else {
+            // Logic for an enabled robot
+            if(m_intakeSupplier.get() == true) {
+                setSingleColor(Color.YELLOW);
+            } else if(m_feederSupplier.get() == true && m_intakeSupplier.get() == false) {
+                setSingleColor(Color.GREEN);
+            } else {
+                setSingleColor(Color.WHITE);
+            }
+        }
     }
 
     public void setSingleColor(int red, int green, int blue) {
@@ -31,33 +81,8 @@ public class LightStrip extends SubsystemBase{
 
         m_leds.setData(m_buffer);
     }
-
-    @Override
-    public void periodic() {
-        // Set color to orange if the robot is not connected to the driver station
-        if(DriverStation.isDSAttached() == false) {
-            setSingleColor(255, 30, 0);
-            return;
-        }
-
-        // If the robot is disabled, display the current alliance if there is one
-        if(DriverStation.isDisabled()) {
-            if(DriverStation.getAlliance().isPresent()) {
-                if(DriverStation.getAlliance().get() == Alliance.Red) {
-                    setSingleColor(255, 0, 0); // Set all LEDs to red if we are disabled on red
-                } else {
-                    setSingleColor(0, 0, 255);
-                }
-            }
-        } else {
-            // Logic for an enabled robot
-            if(m_intakeSupplier.get() == true) {
-                setSingleColor(255, 255, 0); // Yellow
-            } else if(m_feederSupplier.get() == true && m_intakeSupplier.get() == false) {
-                setSingleColor(0, 255, 0); // Green
-            } else {
-                setSingleColor(255, 255, 255); // White
-            }
-        }
+    
+    public void setSingleColor(Color color) {
+        setSingleColor(color.red, color.green, color.blue);
     }
 }
