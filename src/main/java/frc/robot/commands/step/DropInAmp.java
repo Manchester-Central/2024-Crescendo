80216c840b6b90215437d389211334be46b1e165
CHAOS 131 <131.chaos@gmail.com>
2024-05-30 19:10:57 -0400

    Battle Cry refeed on driver controller

--- src/main/java/frc/robot/commands/step/DropInAmp.java
+++ src/main/java/frc/robot/commands/step/DropInAmp.java
@@ -43,7 +43,10 @@ public class DropInAmp extends Command {
   @Override
   public void execute() {
     if (m_spitTimer.hasElapsed(0.25)) {
-      m_lift.moveToHeight(LiftConstants.IntakeHeightMeters);
+      m_lift.moveToHeight(LiftConstants.BattleCryAmpMeters);
+      m_launcher.setTiltAngle(LauncherConstants.BattleCryAmpAngle);
+      m_feeder.setFeederPower(0.5);
+      m_launcher.setLauncherPower(0.5);
       return;
     }
     m_lift.moveToHeight(LiftConstants.AmpMeters);
