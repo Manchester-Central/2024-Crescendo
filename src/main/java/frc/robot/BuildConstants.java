0a67beff33d57f4808750c28059ccd7a395a9ac0
CHAOS 131 <131.chaos@gmail.com>
2025-06-05 20:21:58 -0400

    QuestNav download progress

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2024-Crescendo";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 526;
-    public static final String GIT_SHA = "cca565ad56a957286151d5d7b60bd57e7d3d046d";
-    public static final String GIT_DATE = "2025-02-08 20:00:48 EST";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-06-05 19:56:56 EDT";
-    public static final long BUILD_UNIX_TIME = 1749167816852L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 528;
+    public static final String GIT_SHA = "87d374e1a8afab03d491e4051395df9939a0773b";
+    public static final String GIT_DATE = "2025-06-05 20:00:23 EDT";
+    public static final String GIT_BRANCH = "QuestNav";
+    public static final String BUILD_DATE = "2025-06-05 20:20:43 EDT";
+    public static final long BUILD_UNIX_TIME = 1749169243606L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
